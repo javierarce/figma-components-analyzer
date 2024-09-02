b80ddeeae404d60979f03f3895a4ac80b315ad00
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-09-02 13:20:12 +0200

    Highlights components that use the selected value

--- public/js/property.js
+++ public/js/property.js
@@ -4,6 +4,7 @@ class Property extends Base {
     this.data = data;
     this.fileKey = fileKey;
     this.valueMap = new Map();
+    this.componentMap = new Map();
   }
 
   template() {
@@ -41,7 +42,6 @@ class Property extends Base {
     this.templateData = { ...this.data, fileKey: this.fileKey };
     super.render();
     this.$el.classList.add(`is-${this.data.type.toLowerCase()}`);
-
     this.$el.addEventListener("click", this.handleClick.bind(this));
 
     const componentLinks = this.$el.querySelectorAll(".Component");
@@ -50,8 +50,14 @@ class Property extends Base {
       link.addEventListener("mouseleave", this.handleComponentLeave.bind(this));
     });
 
-    this.createValueMap();
+    const valueSpans = this.$el.querySelectorAll(".Property__value");
+    valueSpans.forEach((span) => {
+      span.addEventListener("mouseenter", this.handleValueHover.bind(this));
+      span.addEventListener("mouseleave", this.handleValueLeave.bind(this));
+    });
 
+    this.createValueMap();
+    this.createComponentMap();
     return this.$el;
   }
 
@@ -61,9 +67,25 @@ class Property extends Base {
         if (componentValues && componentValues.length) {
           this.valueMap.set(componentId, componentValues);
         } else {
-          // If no specific values are provided for this component, use all available values
           this.valueMap.set(componentId, this.data.values || []);
         }
+      },
+    );
+  }
+
+  createComponentMap() {
+    this.data.components.forEach(
+      ([componentName, componentId, componentValues]) => {
+        const values =
+          componentValues && componentValues.length
+            ? componentValues
+            : this.data.values || [];
+        values.forEach((value) => {
+          if (!this.componentMap.has(value)) {
+            this.componentMap.set(value, new Set());
+          }
+          this.componentMap.get(value).add(componentId);
+        });
       },
     );
   }
@@ -71,15 +93,12 @@ class Property extends Base {
   handleComponentHover(e) {
     const componentId = e.target.dataset.componentId;
     const values = this.valueMap.get(componentId);
-
     this.$el.classList.add("is-hover");
-
     if (values && values.length) {
       values.forEach((value) => {
         const valueSpan = this.$el.querySelector(
           `.Property__value[data-value="${value}"]`,
         );
-
         if (valueSpan) {
           valueSpan.classList.add("is-highlighted");
         }
@@ -91,6 +110,26 @@ class Property extends Base {
     this.$el.classList.remove("is-hover");
     const valueSpans = this.$el.querySelectorAll(".Property__value");
     valueSpans.forEach((span) => span.classList.remove("is-highlighted"));
+  }
+
+  handleValueHover(e) {
+    const value = e.target.dataset.value;
+    const componentIds = this.componentMap.get(value);
+    if (componentIds) {
+      componentIds.forEach((componentId) => {
+        const componentLink = this.$el.querySelector(
+          `.Component[data-component-id="${componentId}"]`,
+        );
+        if (componentLink) {
+          componentLink.classList.add("is-highlighted");
+        }
+      });
+    }
+  }
+
+  handleValueLeave(_e) {
+    const componentLinks = this.$el.querySelectorAll(".Component");
+    componentLinks.forEach((link) => link.classList.remove("is-highlighted"));
   }
 
   handleClick(e) {
